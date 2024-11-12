5680938a363abbeb3ac8d73d2459725a11d17a87
paulinasolorzano <32572042+paulinasolorzano@users.noreply.github.com>
2024-11-12 13:48:49 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
       .then(function(registration) {
         console.log('Service Worker registrado con éxito:', registration);
       })
